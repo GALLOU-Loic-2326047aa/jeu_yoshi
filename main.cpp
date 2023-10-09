043323e7cdc722421c8096212571cb8480cbda60
loicgll <146103937+loicgll@users.noreply.github.com>
2023-10-09 11:59:50 +0200

    Update main.cpp
    voila

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
+#include <stdlib.h>     /* srand, rand */
+#include <experimental/random>
 using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    char verif;
+    int petal;
+    string jtm ="je t'aime";
+    string jtmp ="je t'aime pas";
+    string jtmf ="je t'aime à la folie";
+    petal = experimental::randint(9,12);
+    // petal = rand() % 12 + 9; //une autre manière de mettre de l'aléatoire
+    while(petal>0){
+        cout << "appuyer sur une touche" << endl;
+        cout << petal << endl;
+        cin >> verif;
+        if(petal % 2 == 0)
+        {
+            cout  << jtm<<endl;
+        }
+        else
+        {
+            cout << jtmp<<endl;
+        }
+        petal = petal - 1;
+    }
 }
