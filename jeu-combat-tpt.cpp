ac91bf8282cb9c90eed10eced341eb6be98bfa25
loicgll <146103937+loicgll@users.noreply.github.com>
2023-10-12 10:31:50 +0200

    Update jeu-combat-tpt.cpp
    finalise

--- jeu-combat-tpt.cpp
+++ jeu-combat-tpt.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>     /* srand, rand */
-#include <experimental/random>
 using namespace std;
 int main()
 {
@@ -10,27 +8,34 @@ int main()
     int popo (60); 
     int nbj1popo (3);
     int nbj2popo (3);
-    int attackbrute (40);
+    int attackj1 (40);
+    int attackj2 (40);
     float attack;
-    float defensej1 (0.90);
-    float defensej2 (0.90);
+    float defensej1 (0);
+    float defensej2 (10);
     char (j1);
     char (j2);
-
-    while(pvj1> 0 && pvj2>0)
+    string (nomjoueur1);
+    string (nomjoueur2);
+   cout << "entrez votre nom joueur 1" << endl;
+   cin >> nomjoueur1;
+   cout << "entrez votre nom joueur 2"<<endl;
+   cin >> nomjoueur2;
+       while(pvj1> 0 && pvj2>0)
    {
-    cout << "point de vie du joueur 1: " << pvj1 << "  defense du joueur 1: " << (1-defensej1)*100<<"%" << " nombre de potion: "<<nbj1popo << endl;
-    cout << "point de vie du joueur 2: " << pvj2 << "  defense du joueur 2: " << (1-defensej2)*100 <<"%" << " nombre de potion: "<<nbj2popo << endl;
-    cout <<"que voulez vous faire: " <<'\n'<< "attaquer: 1"<< '\n'<<"prendre une potion: 2"<<'\n'<<"se renforcer: 3"<<'\n' ;
+    cout << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' <<"Tour de "<<nomjoueur1<< '\n' << '\n';
+    cout <<"stat de "<<nomjoueur1<< "  point de vie : " << pvj1 <<'\t'<<"attack "<<attackj1<< "  defense: " << 100*defensej1/(defensej1+100)<<"%" <<'\t'<< "  nombre de potion: "<<nbj1popo << endl;
+    cout <<"stat de "<<nomjoueur2<< "  point de vie : " << pvj2 <<'\t'<<"attack "<<attackj2<< "  defense: " << 100*defensej2/(defensej2+100)<<"%" <<'\t'<< "  nombre de potion: "<<nbj2popo <<'\n'<< endl;
+    cout <<"que voulez vous faire: " <<'\n'<< "attaquer: 1"<< '\n'<<"prendre une potion: 2"<<'\n'<<"se renforcer: 3"<<'\n'<<"aiguiser sa lame 4"<<endl;
     cin >> j1 ;
              switch ( j1 )
      {
          case '1':
-           attack= attackbrute*defensej2;
+           attack= attackj1*(1-(defensej2/(defensej2+100)));
             pvj2 = pvj2 - attack;
             break;
          case '2':
-         if (nbj2popo<=0)
+         if (nbj1popo<=0)
             {
                cout << "vous n'avez plus de potion vous perdez votre tour."<<'\n' ;
                break;
@@ -41,7 +46,10 @@ int main()
             break;
             case '3':
             cout<<"se renforcer"<<endl;
-            defensej1 = defensej1 - 0.10;
+            defensej1 = defensej1 + 15;
+            break;
+            case '4':
+            attackj1 = attackj1 +5;
             break;
          default:
          cout <<"erreur mauvais caractère entré"<<endl;
@@ -50,19 +58,17 @@ if (pvj2<=0)
 {
       cout << "le Joueur 1 a gagné"<<endl;
    break;}
-{
-   /* code */
-}
 
-    cout << "point de vie du joueur 1: " << pvj1  << '\t' << "  defense du joueur 1: " << (1-defensej1)*100 << '\t' << "%" << " nombre de potion: "<<nbj1popo << endl;
-    cout << "point de vie du joueur 2: " << pvj2 << "  defense du joueur 2: " << (1-defensej2)*100<<"%" << " nombre de potion: "<<nbj2popo << endl;
-    cout <<"que voulez vous faire: " <<'\n'<< "attaquer: 1"<< '\n'<<"prendre une potion: 2"<<'\n'<<"se renforcer: 3"<<'\n' ;
-    cin >> j2 ;
+
+      cout << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' << '\n' <<"Tour de "<<nomjoueur2<< '\n' << '\n';
+      cout <<"stat de "<<nomjoueur1<< "  point de vie : " << pvj1 <<'\t'<<"attack "<<attackj1<< "  defense: " << 100*defensej1/(defensej1+100)<<"%" <<'\t'<< "  nombre de potion: "<<nbj1popo << endl;
+      cout <<"stat de "<<nomjoueur2<< "  point de vie : " << pvj2 <<'\t'<<"attack "<<attackj2<< "  defense: " << 100*defensej2/(defensej2+100)<<"%" <<'\t'<< "  nombre de potion: "<<nbj2popo <<'\n'<< endl;
+      cout <<"que voulez vous faire: " <<'\n'<< "attaquer: 1"<< '\n'<<"prendre une potion: 2"<<'\n'<<"se renforcer: 3"<<'\n'<<"aiguiser sa lame 4"<<endl;
+      cin >> j2 ;
              switch ( j2 )
      {
          case '1':
-         attack= attackbrute*defensej1;
-            pvj1 = pvj1 - attack;
+            attack= attackj2*(1-(defensej1/(defensej1+100)));            pvj1 = pvj1 - attack;
             break;
          case '2':
             if (nbj2popo<=0)
@@ -75,7 +81,10 @@ if (pvj2<=0)
             cout << "vous avez maintenant " << pvj1 << "point de vie"<< '\n'<<endl;
             break;
             case '3':
-              defensej2 = defensej2 - 0.10;
+              defensej2 = defensej2 +15;
+            break;
+            case '4':
+            attackj2 = attackj2 +5;
             break;
          default:
          cout <<"erreur mauvais caractère entré"<<endl;
@@ -85,16 +94,6 @@ if (pvj2<=0)
    cout << "le Joueur 2 a gagné"<<endl;
    break;
    }
-
-
-
-
-
-
-
-
-
-
     }
 
 }
